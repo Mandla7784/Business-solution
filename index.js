7010280d9dbf30073b32590872e3b40a77e835cd
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-01-02 11:13:55 +0200

    updates

--- index.js
+++ index.js
@@ -25,40 +25,36 @@ const renderPricingCards = async (data) => {
     console.log(error, "Error fetching resources");
   }
 };
-
 /**
- *
- * @param {object} param
- * This is to create the cards by  generating  HTML structure
- *
+ * Creates a pricing card using HTML structure.
+ * @param {object} param - Pricing card data.
  */
 function createPricingCard({ heading, price, description, btnText }) {
   // Create card container
   const card = document.createElement("div");
-  card.className = "pricing-card shadow-lg rounded-lg border p-6 text-center";
+  card.className = "pricing-card";
 
   // Heading
   const headingElement = document.createElement("h3");
-  headingElement.className = "text-2xl font-bold text-gray-800 mb-4";
+  headingElement.className = "pricing-card-heading";
   headingElement.textContent = heading;
   card.appendChild(headingElement);
 
   // Price
   const priceElement = document.createElement("h1");
-  priceElement.className = "text-4xl font-extrabold text-blue-600 mb-4";
-  priceElement.textContent = price === "Contact Us" ? price : `$${price}/mo`;
+  priceElement.className = "pricing-card-price";
+  priceElement.textContent = price === "text us" ? price : `R${price}/mo`;
   card.appendChild(priceElement);
 
   // Description
   const descriptionElement = document.createElement("p");
-  descriptionElement.className = "text-gray-600 mb-6";
+  descriptionElement.className = "pricing-card-description";
   descriptionElement.textContent = description;
   card.appendChild(descriptionElement);
 
   // Button
   const button = document.createElement("button");
-  button.className =
-    "bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition";
+  button.className = "pricing-card-button";
   button.textContent = btnText;
   card.appendChild(button);
 
