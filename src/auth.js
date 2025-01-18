255029a6f4fb802ac37ff7712b5944f1db8a2ae4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-01-18 19:10:54 +0200

    Added firebase configuration

---
+++ src/auth.js
@@ -0,0 +1,9 @@
+console.log("Connected");
+const firebaseConfig = {
+  apiKey: "AIzaSyDZb3TH-U1JNJKMr2wNYHuhg94WVQMSa_A",
+  authDomain: "shoppinglist-8afb5.firebaseapp.com",
+  projectId: "shoppinglist-8afb5",
+  storageBucket: "shoppinglist-8afb5.appspot.com",
+  messagingSenderId: "1053775639867",
+  appId: "1:1053775639867:web:868f64621e0540f7fa731e",
+};
