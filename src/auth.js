12f2af6a76d9103b1db80b5f3a59fec0ad4fedb4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-01-18 19:13:25 +0200

    working on firebase

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,3 @@
-console.log("Connected");
 const firebaseConfig = {
   apiKey: "AIzaSyDZb3TH-U1JNJKMr2wNYHuhg94WVQMSa_A",
   authDomain: "shoppinglist-8afb5.firebaseapp.com",
@@ -7,3 +6,6 @@ const firebaseConfig = {
   messagingSenderId: "1053775639867",
   appId: "1:1053775639867:web:868f64621e0540f7fa731e",
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
